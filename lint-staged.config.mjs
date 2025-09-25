4baf30e946fe5c34e393610a580d08fc94689a57
Luk4x <luk4xm4ci3l@gmail.com>
2025-09-25 09:09:32 -0300

    fix: lint-staged prettier path

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 export default {
   '*.{js,ts,jsx,tsx,css,md}': [
-    'pnpm dlx prettier --write --config ../../.prettierrc',
+    'pnpm dlx prettier --write --config .prettierrc',
   ],
   '*.{js,ts,jsx,tsx}': ['pnpm dlx eslint --fix'],
 };
